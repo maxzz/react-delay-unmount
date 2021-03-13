540bc47c086ac26e9c8c6e9f0711a77274b4fc56
Max Zakharzhevskiy <maxzz@msn.com>
2021-03-13 03:20:49 -0800

    Fix: add a separate vite.config for build

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,20 @@ import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    base: '/react-delay-unmount/',
-    plugins: [reactRefresh()]
-})
+
+export default ({ mode, command }: { mode: string, command: string }) => {
+    //console.log(`mode: ${mode}, command: ${command}`); // mode: development, command: serve
+
+    let commonConfig = defineConfig({
+        plugins: [reactRefresh()]
+    });
+
+    if (command === 'build') {
+        commonConfig = defineConfig({
+            ...commonConfig,
+            base: '/react-delay-unmount/',
+        });
+    }
+    
+    return commonConfig;
+}
