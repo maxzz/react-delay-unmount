c04a784ed2aced777aea6bf96dc8e28caf5772cb
Max Zakharzhevskiy <maxzz@msn.com>
2022-12-26 00:25:41 -0800

    WIP. update DelayedWithState component

--- src/components/DelayedWithState.tsx
+++ src/components/DelayedWithState.tsx
@@ -3,14 +3,14 @@ import React from 'react';
 type DelayUnmountingProps = {
     isMounted: boolean;
     delayTime: number;
-}
+};
 
 type DelayUnmountingState = {
     shouldRender: boolean;
-}
+};
 
 function delayUnmounting(Component: Function) {
-    return class extends React.Component<DelayUnmountingProps, DelayUnmountingState> {
+    return class DelayedComponent extends React.Component<DelayUnmountingProps, DelayUnmountingState> {
         state = {
             shouldRender: this.props.isMounted
         };
@@ -18,25 +18,25 @@ function delayUnmounting(Component: Function) {
         componentDidUpdate(prevProps: DelayUnmountingProps) {
             if (prevProps.isMounted && !this.props.isMounted) {
                 setTimeout(() => {
-                    this.setState({shouldRender: false});
+                    this.setState({ shouldRender: false });
                 }, this.props.delayTime);
             } else if (!prevProps.isMounted && this.props.isMounted) {
-                this.setState({shouldRender: true});
+                this.setState({ shouldRender: true });
             }
         }
 
         render() {
-            return this.state.shouldRender ? <Component {...this.props} /> : null;
+            return this.state.shouldRender
+                ? <Component {...this.props} />
+                : null;
         }
     };
 }
 
-function BoxDialog(props: DelayUnmountingProps) {
+function BoxDialog({ isMounted }: DelayUnmountingProps) {
     return (
-        <div style={{animation: props.isMounted ? 'demo-bounce-in 1s' : 'demo-bounce-out 1s'}}>
-            <div
-                className="animate-bounce text-center"
-            >
+        <div style={{ animation: isMounted ? 'demo-bounce-in 1s' : 'demo-bounce-out 1s' }}>
+            <div className="animate-bounce text-center">
                 ✨✨✨✨✨✨
                 ✨✨✨✨✨✨
             </div>
@@ -48,23 +48,25 @@ const DelayUnmounting = delayUnmounting(BoxDialog);
 
 type DemoState = {
     isMounted: boolean;
-}
+};
 
 export class DelayedWithState extends React.Component<{}, DemoState> {
     state = {
         isMounted: false,
-    }
+    };
 
     toggle = () => {
-        this.setState((state) => ({ isMounted: !state.isMounted}));
-    }
+        this.setState((state) => ({ isMounted: !state.isMounted }));
+    };
 
     render() {
         return (
             <div className="flex">
-                <button onClick={this.toggle} className="px-2 py-1 border rounded border-gray-200 text-gray-100 hover:bg-gray-700 focus:outline-none">
-                    Toggle
+
+                <button className="px-4 py-1 border rounded border-gray-200 text-gray-100 hover:bg-gray-700 focus:outline-none" onClick={this.toggle}>
+                    Toggle w/ class
                 </button>
+
                 <div className="w-20">
                     <DelayUnmounting isMounted={this.state.isMounted} delayTime={1000} />
                 </div>
