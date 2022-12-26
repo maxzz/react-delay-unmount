c22609f85fdffe2a3649471b30c66c25fc70fe10
Max Zakharzhevskiy <maxzz@msn.com>
2022-12-26 00:02:32 -0800

    WIP. add aliases support

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,9 @@ export default defineConfig({
     server: {
         port: 3000,
     },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        }
+    }
 });
