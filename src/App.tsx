1f317cd63d3862adddf9538de2c289c1e4feaff2
Max Zakharzhevskiy <maxzz@msn.com>
2023-01-01 17:20:29 -0800

    WIP. move layout definition to tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { DelayedWithHooks } from './components/DelayedWithHooks';
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
+            <header className="min-h-screen text-[length:calc(10px+2vmin)] text-white bg-[#282c34] flex flex-col items-center justify-center">
                 <div className="grid auto-rows-auto gap-1">
                     <DelayedWithState />
                     <DelayedWithHooks />
