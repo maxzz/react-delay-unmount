9e348f08275f50a3def554b99457a46bbf92a27b
Max Zakharzhevskiy <maxzz@msn.com>
2023-01-01 18:27:07 -0800

    WIP. app. fix grid responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export function App() {
     return (
         <div className="App">
             <header className="min-h-screen text-[length:calc(10px+2vmin)] text-white bg-zinc-800 flex flex-col items-center justify-center">
-                <div className="grid grid-cols-3 grid-rows-[50%_50%] gap-24">
+                <div className="p-4 grid grid-cols-[1fr_minmax(20ch,30ch)_1fr]  gap-y-4">
 
                     <div className="col-start-2">
                         <DelayedWithHooks />
