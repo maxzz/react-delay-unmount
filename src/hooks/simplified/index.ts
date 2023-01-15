897e8e8286582c89e8ea410fbacc8012bd2f06d3
Max Zakharzhevskiy <maxzz@msn.com>
2023-01-14 20:46:31 -0800

    WIP. hooks. update index access

--- src/hooks/simplified/index.ts
+++ src/hooks/simplified/index.ts
@@ -1,8 +1,7 @@
-// inspired by https://www.youtube.com/watch?v=0c6znExIqRw '5 Custom React Hooks You Need In Every Project'
-// inspired by https://www.youtube.com/watch?v=vrIxu-kfAUo 'Save Hours Of Work With These 5 Custom React Hooks'
-
 export * from './useTimeout';
 export * from './useDebounce';
 export * from './useUpdateEffect';
 export * from './useArray';
 export * from './useToggle';
+export * from './useAsync';
+export * from './useFetch';
