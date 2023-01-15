c4d9324fcba7f4c9a2a01cfda3ad586fde107625
Max Zakharzhevskiy <maxzz@msn.com>
2023-01-14 20:46:44 -0800

    WIP. hooks. add links

--- src/hooks/simplified/index.ts
+++ src/hooks/simplified/index.ts
@@ -1,3 +1,10 @@
+// refs:
+// https://www.youtube.com/watch?v=0c6znExIqRw&list=PLZlA0Gpn_vH-aEDXnaFNLsqiJWFpIWV03&index=1 '5 Custom React Hooks You Need In Every Project'
+// https://www.youtube.com/watch?v=vrIxu-kfAUo&list=PLZlA0Gpn_vH-aEDXnaFNLsqiJWFpIWV03&index=3 'Save Hours Of Work With These 5 Custom React Hooks'
+// https://www.youtube.com/watch?v=Ix_xeCuS4XA&list=PLZlA0Gpn_vH-aEDXnaFNLsqiJWFpIWV03&index=4 'Make React Easy With These 5 Custom React Hooks'
+// https://www.youtube.com/watch?v=XEQ9nEHYIbw&list=PLZlA0Gpn_vH-aEDXnaFNLsqiJWFpIWV03&index=5 'Every Big React App Needs These 5 Custom Hooks'
+// https://www.youtube.com/watch?v=wvJPGUuEUPk&list=PLZlA0Gpn_vH-aEDXnaFNLsqiJWFpIWV03&index=6 'Save Hours Debugging With These React Hooks'
+
 export * from './useTimeout';
 export * from './useDebounce';
 export * from './useUpdateEffect';
